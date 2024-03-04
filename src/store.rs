20ca95843e48c1164d9334fcac3527e8bae1a5a1
jokemanfire <hu.dingyang@zte.com.cn>
2024-03-04 19:57:22 +0800

    modify some path
    Signed-off-by: jokemanfire <hu.dingyang@zte.com.cn>

--- src/store.rs
+++ src/store.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 use oci_spec::{image::Descriptor, runtime::Mount};
 use std::time::SystemTime;
 use crate::common::string_to_systemtime;
-type digest=String;
+type Digest=String;
 pub struct Store{
     root:String,
 }
@@ -14,13 +14,13 @@ pub struct Status{
     pub dref:String,
     pub offset:i64,
     pub total:i64,
-    pub expected:digest,
+    pub expected:Digest,
     pub start_at:SystemTime,
     pub update_at:SystemTime,
 }
 
 pub struct Info{
-    pub digest:digest,
+    pub digest:Digest,
 	pub size   :u64,
 	pub created_at:SystemTime,
 	pub updated_at :SystemTime,
@@ -38,7 +38,7 @@ impl Store {
             root:"/".to_string(),
          }
     }
-    pub fn info(&self, dgst:digest)->Result<Info,Error> {
+    pub fn info(&self, dgst:Digest)->Result<Info,Error> {
         let p = self.blobpath(dgst.clone()).unwrap();
         let metadata  = fs::metadata(p).unwrap();
         
@@ -51,23 +51,23 @@ impl Store {
         })
     } 
 
-    pub fn blobpath(&self, dgst:digest) -> Result<String,Error>{
-        let str_path = self.root.clone() + "/blobs/" + dgst.as_str();
+    pub fn blobpath(&self, dgst:Digest) -> Result<String,Error>{
+        let str_path = Path::new(&self.root.clone()).join("blobs").join(dgst).to_string_lossy().to_string();
         Ok(str_path)
         
     }
-    pub fn read_at(&self, dgst:digest) -> Result<BufReader<File>,Error> {
+    pub fn read_at(&self, dgst:Digest) -> Result<BufReader<File>,Error> {
         let p = self.blobpath(dgst).unwrap();
         let file = File::open(p).unwrap();
         let reader = BufReader::new(file);
         Ok(reader)
     }
-    pub fn delete(&self,dgst:digest){
+    pub fn delete(&self,dgst:Digest){
         let p = self.blobpath(dgst).unwrap();
-        fs::remove_dir_all(p);
+        let _ = fs::remove_dir_all(p);
 
     }
-    pub fn update(&self,dgst:digest){
+    pub fn update(&self,dgst:Digest){
         let mut info = self.info(dgst).unwrap();
         info.updated_at = SystemTime::now();
         //todo ! modify file created time
@@ -77,10 +77,15 @@ impl Store {
     }
     // try walk all file
     pub fn walk(&self,filters:Vec<String>){
-        let p = self.root.clone() + "/blobs";
+
+        let root = Path::new(&self.root.clone()).join("blobs").to_string_lossy().to_string();
         //hash validate
         todo!()
         
+    }
+
+    fn resumestatus(){
+        todo!()
     }
 
     fn total(&self,ingestPath:String)->i64{
@@ -89,17 +94,26 @@ impl Store {
         r
     }
 
-    pub fn writer(&self,mref:String, total:i64,expected:digest)-> Result<BufWriter<File>,Error>{
+    pub fn writer(&self,mref:String, total:i64,expected:Digest)-> Result<BufWriter<File>,Error>{
         //writer should hold One should lock it
         if expected != ""{
             let p = self.blobpath(expected).unwrap();
 
         }
-        let ingest_path = self.ingest_root(mref);
-        // let rp = 
+        let (path,refp,data) = self.ingest_paths(mref);
+        // ensure that the ingest path has been created. todo()!
+        let file = File::open(data).unwrap();
+        let writer = BufWriter::new(file);
 
-        todo!()
+        Ok(writer)
 
+    }
+
+    fn ingest_paths(&self,mref:String)-> (String,String,String){
+        let fp = self.ingest_root(mref);
+        let rp = Path::new(&fp.clone()).join("ref").to_string_lossy().to_string();
+        let dp = Path::new(&fp.clone()).join("data").to_string_lossy().to_string();
+        return (fp,rp,dp)
     }
 
     pub fn status(&self,mref:String)->Option<Status>{
@@ -118,7 +132,7 @@ impl Store {
         Some(Status { dref: refdata, offset: dp.len() as i64, total: self.total(ingest_path), expected: String::default(), start_at: startat, update_at: updateat })
     }
     fn ingest_root(&self, mref:String)->String{
-        self.root.clone() + "/ingest/"+mref.as_str()
+        Path::new(&self.root.clone()).join("ingest").join(mref).to_string_lossy().to_string()
     }
 
 }
